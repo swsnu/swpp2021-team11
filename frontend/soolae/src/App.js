87c1be1710fc4955d9b2ac2d7f37d877c4460642
bumkyu00 <bumkyu00@naver.com>
2021-10-22 00:43:11 +0900

    modified:   frontend/soolae/package.json 	modified:   frontend/soolae/src/App.js 	modified:   frontend/soolae/src/index.js 	modified:   frontend/soolae/yarn.lock 	frontend/soolae/node_modules/ 	frontend/soolae/src/containers/ 	frontend/soolae/src/serviceWorker.js

--- frontend/soolae/src/App.js
+++ frontend/soolae/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-function App() {
+import Login from './containers/login';
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConnectedRouter history={props.history}>
+      <div className="App" >
+        <Switch>
+          <Route path='/login' exact render={() => <Login />} />
+          <Redirect exact from='/' to='login' />
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </div >
+    </ConnectedRouter>
   );
 }
 
