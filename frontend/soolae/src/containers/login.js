d96aec356b34ca42cf7f4f59884c2ae68f7819f7
bumkyu00 <bumkyu00@naver.com>
2021-11-01 16:02:37 +0900

    new file:   .gitignore 	new file:   frontend/soolae/src/containers/login.js 	new file:   frontend/soolae/src/serviceWorker.js

---
+++ frontend/soolae/src/containers/login.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+class Login extends Component {
+    state={email:'', password:''}
+
+    render() {
+        return (
+            <div className="Login">
+                <dir>
+                    <h1>Login Page</h1>
+                    <label>Email:</label>
+                    <input id="email-input" type="email" value={this.state.email}
+                        onChange={(event) => this.setState({ email: event.target.value })}>
+                    </input>
+                </dir>
+                <dir>
+                    <label>Password:</label>
+                    <input id="pw-input" type="password" value={this.state.password}
+                        onChange={(event) => this.setState({ password: event.target.value })}>
+                    </input>
+                </dir>
+                <button id="login-button">Log in</button>
+            </div>
+        );
+    }
+}
+
+  export default Login;
