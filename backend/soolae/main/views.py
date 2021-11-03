82727ee7eea645a79fc222b551e0dac2bab902f1
bumkyu00 <bumkyu00@naver.com>
2021-11-04 01:25:06 +0900

    modified:   backend/soolae/main/views.py

--- backend/soolae/main/views.py
+++ backend/soolae/main/views.py
@@ -1,6 +1,8 @@
 # pylint: disable=unused-argument
 from django.http import HttpResponse ,JsonResponse
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET"])
 def recommend(request):
     result = [
         {
