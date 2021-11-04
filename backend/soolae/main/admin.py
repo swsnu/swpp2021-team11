e21ebfde224e643e6f9a3f8b880870f847014ecb
Hong Soona <hongsoona@snu.ac.kr>
2021-11-04 13:15:18 +0000

    Add Admin

--- backend/soolae/main/admin.py
+++ backend/soolae/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+modelList = [SoolMaterial, SoolMaker, SoolTag, SoolCategory, TasteStandard, Anju, Sool]
+for i in modelList:
+    admin.site.register(i)
 
 # Register your models here.
