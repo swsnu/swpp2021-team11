7fa05a4580668fc975e1809d7a922a0de87ad924
3lna7 <3lna796@gmail.com>
2021-11-24 21:00:57 +0900

    Refactoring ofSignIn SignUp and SignOut

--- frontend/soolae/src/pages/signUp.js
+++ frontend/soolae/src/pages/signUp.js
@@ -1,103 +1,49 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import * as actionCreators from '../store/actions/actionCreators';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-// axios.defaults.xsrfCookieName = 'csrftoken';
-// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-class SignUpPage extends React.Component {
+
+class SignUp extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
             username: '',
             email: '',
-            password: '',
+            password: ''
         };
     }
     register() {
-        const data = {username: this.state.username, email: this.state.email, password: this.state.password};
-        fetch('/api/signup/', {
-            credentials: 'include',
-            method: 'POST',
-            mode: 'same-origin',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                // 'X-CSRFToken': csrftoken
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+        
+        const data = {'username': this.state.username, 'email': this.state.email, 'password': this.state.password};
+        this.props.signUp(data);
 
-        alert('Account made successfuly!');
-        // result = await result.json();
-        // console.log('result',result);
-
-        // let item = {username: this.state.username, password: this.state.password};
-        // axios.post('http://localhost:8000/api/signup/', item);
-        // // console.log(item);
-
-        // // let result = await fetch('http://locallhost:8000/api/signup',{
-        // //     method: 'POST',
-        // //     body: JSON.stringify(item),
-        // //     headers:{
-        // //         'Content-Type':'application/json',
-        // //         'Accept':'application/json'
-        // //     }
-        // // });
-        // // result = await result.json();
-        // // console.log('result',result);
-
-        // //var querystring = require('querystring');
     }
 
     render() {
         return (
-            <div className="signup">
+            <div className='signup'>
                 <h1>Sign Up</h1>
-                <dir className="form">
-                    <dir className="form-group">
+                <dir className='form'>
+                    <dir className='form-group'>
                         <label>Username</label>
-                        <input
-                            type="text"
-                            value={this.state.username}
-                            placeholder="username"
-                            onChange={(e) => this.setState({username: e.target.value})}
-                        />
+                        <input type='text' value={this.state.username} placeholder='username' onChange={(e) => this.setState({ username: e.target.value })} />
                     </dir>
-                    <dir className="form-group">
+                    <dir className='form-group'>
                         <label>Email</label>
-                        <input
-                            type="text"
-                            value={this.state.email}
-                            placeholder="email"
-                            onChange={(e) => this.setState({email: e.target.value})}
-                        />
+                        <input type='text' value={this.state.email} placeholder='email' onChange={(e) => this.setState({ email: e.target.value })} />
                     </dir>
-                    <dir className="form-group">
+                    <dir className='form-group'>
                         <label>Password</label>
-                        <input
-                            type="password"
-                            value={this.state.password}
-                            placeholder="password"
-                            onChange={(e) => this.setState({password: e.target.value})}
-                        />
+                        <input type='password' value={this.state.password} placeholder='password' onChange={(e) => this.setState({ password: e.target.value })} />
                     </dir>
                 </dir>
-                <dir className="footer">
-                    <button
-                        onClick={() => {
-                            this.register();
-                        }}
-                    >
-                        Sign Up
-                    </button>
+                <dir className='footer'>
+                    <button onClick= {() => {this.register();}}>Sign Up</button>
                 </dir>
                 <dir>
                     <p>
@@ -109,4 +55,11 @@ class SignUpPage extends React.Component {
         );
     }
 }
-export default SignUpPage;
+
+const mapDispatchToProps = dispatch => {
+    return {
+        signUp: (data) => dispatch(actionCreators.signUp(data)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(SignUp);
